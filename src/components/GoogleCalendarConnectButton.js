14b48f8b694a3ff224f0962140a119e84ddbeb7b
dillondesilva <dillon.desilva@geeksdiary.com>
2023-08-27 03:11:23 +1000

    Added Google Auth functionality via button

--- src/components/GoogleCalendarConnectButton.js
+++ src/components/GoogleCalendarConnectButton.js
@@ -1,5 +1,19 @@
-import { Text, Pressable, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
+// import GoogleAuth from 'google-auth-library';
+// import GoogleApis from "googleapis";
+// Importing for Google Authentication
+// const authenticate = require('@google-cloud/local-auth');
+// const google = require('googleapis');
+// const process = require('process');
+// const path = require('path');
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+  } from '@react-native-google-signin/google-signin';
+// const SCOPES = ["https://www.googleapis.com/auth/calendar.addons.execute"]
+// const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 // Component styles
 const styles = StyleSheet.create({
@@ -32,20 +46,43 @@ const styles = StyleSheet.create({
     },
 });
 
-function onPressFunction() {
+// Connects to Google Calendar
+async function connectGoogleCalendar() {
+    GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/calendar.addons.execute'], // what API you want to access on behalf of the user, default is email and profile
+        iosClientId: "845570352922-chd6r795jt4lk0mog8chvqvev5fhot7q.apps.googleusercontent.com"
+    });
 
+    const userInfo = await GoogleSignin.signIn();
+    // client = await authenticate({
+    //     scopes: SCOPES,
+    //     keyfilePath: CREDENTIALS_PATH,
+    // });
+    // if (client.credentials) {
+    // await saveCredentials(client);
+    // }
+
+    // const auth = new GoogleAuth({
+    //     scopes: 'https://www.googleapis.com/auth/calendar.addons.execute'
+    // });
+
+    // const client = await auth.getClient();
+    // const projectId = await auth.getProjectId();
+    // const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
+    // const res = await client.request({ url });
+    // console.log(res.data);
 }
 
 // Functional component
 export default function GoogleCalendarConnectButton() {
     return (
         <View style={styles.componentWrapper}>
-            <Pressable style={styles.button} onPress={onPressFunction}>
+            <TouchableHighlight underlayColor="#DDDDDD" style={styles.button} onPress={connectGoogleCalendar}>
                 <View style={styles.buttonContentsWrapper}>
                     <Icon name="google" size={20} color="#000000" style={styles.googleIcon}/>
                     <Text style={styles.buttonText}>Connect to Google Calendar</Text>
                 </View>
-            </Pressable>
+            </TouchableHighlight>
         </View>
     )
 }
