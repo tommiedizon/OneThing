0ff6ad5f132db2ab6b0ee7745d3cc1d1f06576dc
dillondesilva <dillon.desilva@geeksdiary.com>
2023-08-27 11:58:15 +1000

    Final production version

--- src/Emoji.js
+++ src/Emoji.js
@@ -1,10 +1,18 @@
-import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Pressable, StyleSheet, Text, View, Button, onPressLearnMore } from 'react-native';
-import EmojiSelector from 'react-native-emoji-selector';
+// import EmojiSelector from 'react-native-emoji-selector';
+import React, { useState } from 'react';
+import EmojiSelector, { Categories } from 'react-native-emoji-selector';
+import EmojiPicker from './EmojiPicker';
 
 
 export default function Emoji(props) {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+  const toggleEmojiSelector = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
   const proceed = () => {
     props.navigation.navigate('Scheduler');
   }
@@ -15,38 +23,36 @@ export default function Emoji(props) {
 
   return (
       <View style={styles.container}>
-        <View style={styles.name}>
-          <Text style={{ fontSize: 25 }}>
-            <Text>Hi,</Text>
-            <Text style={{ fontWeight: 'bold' }}>Tommie</Text>
-          </Text>
+      <View style={styles.name}>
+        <Text style={{ fontSize: 25 }}>
+          <Text>Hi,</Text>
+          <Text style={{ fontWeight: 'bold' }}>Tommie</Text>
+        </Text>
+      </View>
+
+      <View style={styles.centerContent}>
+        <Text style={{ fontSize: 25, width: 300, textAlign: 'center' }}>
+          <Text>Add an emoji for your </Text>
+          <Text style={{ fontWeight: 'bold' }}>OneThing </Text>
+        </Text>
+        <Text style={{ textAlign: 'center', fontSize: 100 }}>&#x1F3C0;</Text>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ borderWidth: 1, borderRadius: 20, borderColor: '#232244', width: 130 }}>
+            <Button onPress={toggleEmojiSelector} title="Add emoji" color="#232244" />
+          </View>
         </View>
 
-        <View style={styles.centerContent}>
-          <Text style={{ fontSize: 25, width: 300, textAlign: 'center' }}>
-            <Text>Add an emoji for your </Text>
-            <Text style={{ fontWeight: 'bold' }}>OneThing </Text>
-          </Text>
-          <Text style={{ textAlign: 'center', fontSize: 100 }}>&#x1F3C0;</Text>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ borderWidth: 1, borderRadius: 20, borderColor: '#232244', width: 130 }}>
-              <Button
-                onPress={onPressLearnMore}
-                title="Add emoji"
-                color="#232244"
-              />
-            </View>
-          </View>
-          
-          {/* <EmojiSelector onEmojiSelected={emoji => console.log(emoji)}/> */}
+        {/* {showEVmojiPicker && (
+          <EmojiPicker />
+        )} */}
+
           <Pressable onPress={proceed}>
             <View>
               <Icon style={{ marginTop: 50, fontSize: 50, textAlign: 'center' }} name='arrow-circle-right' />
             </View>
           </Pressable>
-          {/* <Icon style={{ marginTop: 50, fontSize: 50, textAlign: 'center' }} name='arrow-circle-right' /> */}
-        </View>
       </View>
+    </View>
   );
 }
 
